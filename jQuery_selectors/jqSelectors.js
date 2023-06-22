54f930036c81c1cea339361d14423e151c5bcfa0
Alex Schmidt <sa-sch12@mail.ru>
2023-06-22 19:47:52 +0300

    jQuery Lesson 2

---
+++ jQuery_selectors/jqSelectors.js
@@ -0,0 +1,14 @@
+function func1() {
+    // jQuery -> $(element name).action()
+    // js -> document.getElementByTagName(element name)
+
+    // $("div").css('background-color', 'orange') Tag name
+    // $('.para').css('font-style', 'italic') class name
+    // $('#para').css('font-style', 'italic') by ID
+    // $('#div1,#div3').css('background-color', 'green') 2 or more id's
+    // $('#div1,li').css('background-color', 'purple') id or calsses or tagNames
+    // $('div > p').fadeToggle()  change inside parent element
+    // $('p:first').fadeToggle() first element in document
+    // $('li:even').fadeToggle() pick even elements 0 1 2 3 ...
+    // $('li:odd').fadeToggle() pick odd elements 0 1 2 3 ...
+}
