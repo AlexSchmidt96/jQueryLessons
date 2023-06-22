db745cea8ef920214b2e45a58bc68a20dced4316
Alex Schmidt <sa-sch12@mail.ru>
2023-06-22 20:45:23 +0300

    jQueryEvents part 2

---
+++ jQuery Events/jQueryEvents2.js
@@ -0,0 +1,21 @@
+$('document').ready(function () {
+    // $('#btn').dblclick(function () {
+    //     $('#img1').css('width', '500px')
+    // }) dbl click function
+
+    // $('#img1').mouseenter(function () {
+    //     $('#img1').css('width', '500px')
+    // }) mouse hover function
+
+    // $('#img1').mouseleave(function () {
+    //     $('#img1').css('width', '250px')
+    // }) mouse hover leave function
+
+    // $('#img1').hover(func1, func2)  function of hover/leaver
+    // function func1() {
+    //     $('#img1').css('width', '500px')
+    // }
+    // function func2() {
+    //     $('#img1').css('width', '250px')
+    // }
+}) 
